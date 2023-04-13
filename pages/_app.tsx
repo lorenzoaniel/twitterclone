a44ffcf80ea56a0b28a9c7d60634c879cee93348
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-13 15:17:45 -0600

    Modal: still need to implement close and submit func also usestate?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import Layout from "@/components/layout/Layout";
+import Modal from "@/components/modal/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<Modal
+				onClose={function (): void {
+					throw new Error("Function not implemented.");
+				}}
+				onSubmit={function (): void {
+					throw new Error("Function not implemented.");
+				}}
+				actionLabel={"Submit"}
+				isOpen
+				title="Test Modal"
+			/>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	);
 }
