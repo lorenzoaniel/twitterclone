06e203720c978c80189b7764a0a758e4c692508e
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-14 19:18:59 -0600

    Create Input.tsx

---
+++ components/input/Input.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface Props {
+	placeholder?: string;
+	value?: string;
+	type?: string;
+	disabled?: boolean;
+	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<Props> = ({ placeholder, value, type, disabled, onChange }) => {
+	return (
+		<input
+			disabled={disabled}
+			onChange={onChange}
+			value={value}
+			type={type}
+			placeholder={placeholder}
+			className={`
+        w-full
+        p-4
+        text-lg
+        bg-black
+        border-2
+        border-neutral-800
+        rounded-md
+        outline-none
+        text-white
+        focus:border-sky-500
+        focus:border-2
+        transition
+        disabled:bg-neutral-900
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+      `}
+		/>
+	);
+};
+
+export default Input;
